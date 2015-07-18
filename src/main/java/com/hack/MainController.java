21ba2f254db95c19a81838f390645d8837089640
wkorando <wkorando@KSL-5NMLL32.ntst.com>
2015-07-18 12:27:25 -0500

    Initial spring boot setup

---
+++ src/main/java/com/hack/MainController.java
@@ -0,0 +1,18 @@
+package com.hack;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MainController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Greg Still Sucks!";
+    }
+
+}
