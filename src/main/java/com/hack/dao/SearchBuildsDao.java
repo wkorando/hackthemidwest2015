ba25ab93015c930de8c8a144594e12f2fdb89801
Greg <AlyssaKnox@alyssas-mbp.jccc.wifi>
2015-07-18 23:35:13 -0500

    fixed entity manager

--- src/main/java/com/hack/dao/SearchBuildsDao.java
+++ src/main/java/com/hack/dao/SearchBuildsDao.java
@@ -1,11 +1,15 @@
 package com.hack.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchBuildsDao {
-	private EntityManager entityManager = EntityManagerUtil.getEntityManager();
+	
+	@PersistenceContext(unitName = "hack", type = PersistenceContextType.EXTENDED)
+	private EntityManager entityManager;
 	
 }
