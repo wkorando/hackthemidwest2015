7a8c85fc0ea5edae63a6bfe330bac1f366d26d8a
Greg <AlyssaKnox@alyssas-mbp.jccc.wifi>
2015-07-18 23:30:55 -0500

    fixed import

--- src/main/java/com/hack/dao/SearchBuildsDao.java
+++ src/main/java/com/hack/dao/SearchBuildsDao.java
@@ -1,9 +1,11 @@
 package com.hack.dao;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchBuildsDao {
-
+	private EntityManager entityManager = EntityManagerUtil.getEntityManager();
 	
 }
