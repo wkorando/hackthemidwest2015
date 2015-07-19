53849c526131271bef8ea141c3605e85a39c19f3
wkorando <wkorando@KSL-5NMLL32.ntst.com>
2015-07-19 02:36:17 -0500

    Test example

--- src/main/java/com/hack/web/services/RestEsoBuilderDataService.java
+++ src/main/java/com/hack/web/services/RestEsoBuilderDataService.java
@@ -1,5 +1,12 @@
 package com.hack.web.services;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/esoBuilderData")
 public class RestEsoBuilderDataService {
+	@PersistenceContext(unitName = "hack", type = PersistenceContextType.EXTENDED)
+	private EntityManager entityManager;
 
-	@RequestMapping( method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String[] searchBuilds(@RequestParam(value="type") String type) {
-		String[] types = new String[] {type};
-		return types;
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String[] searchBuilds(@RequestParam(value = "type") String type,
+			@RequestParam(value = "name") String name) {
+		test();
+		List<String> fieldNames = entityManager.createQuery(
+				"select " + name + " from " + type).getResultList();
+
+		return fieldNames.toArray(new String[] {});
+	}
+	
+	public void test(){
+		System.out.println(Arrays.toString(entityManager.createQuery(
+				" from FACTION").getResultList().toArray()));
 	}
 }
